3b21299cb3a5aaad5b9580677bac964e7f033d16
mitul-open-wallet <contact@openwallet.xyz>
2025-01-15 19:26:52 +0200

    Fix error

--- src/local-test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const GasCalculator_1 = require("./services/GasCalculator");
-// const gasEstimate = new GasEstimation();
-// gasEstimate.fetchGasPrice();
-// const priceService = new AssetPrice();
-// priceService.fetchNativeAssetPrice();
-const gasCalculator = new GasCalculator_1.GasCalculator();
-console.log(gasCalculator.compute());
